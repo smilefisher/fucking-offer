958bfed0c3bc5ae34b96c5d609e4a35a7a6e3437
smilefisher <905488821@qq.com>
2022-12-08 11:54:42 +0800

    1

--- binary-tree/main.go
+++ binary-tree/main.go
@@ -2,7 +2,66 @@ package main
 
 import "fmt"
 
+type TreeNode struct {
+	data  int
+	left  *TreeNode
+	right *TreeNode
+}
+
 func main() {
 
-	fmt.Println("hello world ")
+	var node = &TreeNode{
+		data: 1,
+		left: &TreeNode{
+			data: 2,
+			left: &TreeNode{
+				data: 4,
+			},
+			right: &TreeNode{
+				data: 5,
+				left: &TreeNode{
+					data: 7,
+				},
+				right: &TreeNode{
+					data: 8,
+				},
+			},
+		},
+		right: &TreeNode{
+			data: 3,
+			right: &TreeNode{
+				data: 6,
+			},
+		},
+	}
+
+	// recursive(node)
+	levelTraverse(node)
+}
+
+func recursive(node *TreeNode) {
+	if node == nil {
+		return
+	}
+	// fmt.Println(node.data) //前序 根节点在前遍历
+	recursive(node.left)
+	// fmt.Println(node.data) //中序 根节点在遍历
+	recursive(node.right)
+	fmt.Println(node.data) //后续遍历
+}
+
+
+func levelTraverse(node *TreeNode) {
+	var queen = make(chan *TreeNode, 10)
+	queen <- node
+	for len(queen) > 0 {
+		v := <-queen
+		fmt.Println(v.data)
+		if v.left != nil {
+			queen <- v.left
+		}
+		if v.right != nil {
+			queen <- v.right
+		}
+	}
 }
